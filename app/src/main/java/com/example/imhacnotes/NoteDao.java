cc15b675411667b5117670c0828139ac661002d5
mhammad35 <hammad.sial35@gmail.com>
2021-12-31 18:30:10 +0500

    Add all my files

---
+++ app/src/main/java/com/example/imhacnotes/NoteDao.java
@@ -0,0 +1,28 @@
+package com.example.imhacnotes;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface NoteDao {
+
+    @Insert
+    void Insert(Note note);
+
+    @Update
+    void Update(Note note);
+
+    @Delete
+    void delete(Note note);
+
+    @Query("SELECT * FROM  note_table ORDER BY id ASC")
+    LiveData<List<Note>>getAllNotes();
+
+
+}
